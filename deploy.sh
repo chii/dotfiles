8085f163297504e64d2e1f623ac0e6c8acea2133
Chii <chihironitasaka@gmail.com>
2021-01-05 21:02:15 -0800

    add .vimrc

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .zprofile)
+dotfiles=(.zshrc .zprofile .vimrc)
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
         ln -svf $HOME/dotfiles/$file ~/
