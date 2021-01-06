40f6056bd4ecf9f225ecf978d60ab02bcc116db7
Chii <chihironitasaka@gmail.com>
2021-01-05 20:11:58 -0800

    modify comment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # 配置したい設定ファイル
 dotfiles=(.zshrc .zprofile)
-
-# .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
         ln -svf $HOME/dotfiles/$file ~/
