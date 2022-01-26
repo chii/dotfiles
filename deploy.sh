e1d8fb344032c936307deda083863933a5d73ede
Chihiro Nitasaka <chihironitasaka@chihiro-nitasaka-macbook-pro.local>
2022-01-26 15:38:15 -0800

    comment out homebrew install command

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # brew がインストールされていなければインストール
-if [ -z "$(command -v brew)" ]; then
-    echo "--- Install Homebrew is Start! ---"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew bundle
-    echo "--- Install Homebrew is Done!  ---"
-fi
+#if [ -z "$(command -v brew)" ]; then
+#    echo "--- Install Homebrew is Start! ---"
+#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#    brew bundle
+#    echo "--- Install Homebrew is Done!  ---"
+#fi
 
 # 配置したい設定ファイル
 dotfiles=(.zshrc .zprofile .vimrc .asdfrc)
