7a4ce34c6e0c19aa1909b4d3a29a79aa62ca5048
Chihiro Nitasaka <chihironitasaka@gmail.com>
2022-01-26 16:21:45 -0800

    modify zinit github repo name and add TODO commit for asdf

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,5 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k
-zinit light zdharma/fast-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
 zinit light paulirish/git-open
